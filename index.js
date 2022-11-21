a51e7b24e9364cdbb519fdc345357494912bba3b
Anne Lund <annedeveloper@hotmail.com>
2022-11-21 13:33:30 +0100

    ...

--- index.js
+++ index.js
@@ -10,24 +10,59 @@ const {CarsRouter} = require('./Routes/cars.router.js')
 const {BrandsRouter} = require('./Routes/brands.router.js')
 const {CategoriesRouter} = require('./Routes/categories.router.js')
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+
+
 app.use(function (req, res, next) {
-    res.setHeader("Acces-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    )
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Content-Type, Authorization, Accept, Accept-Language, X-Authorization, X-Requested-With"
-    )
-    res.setHeader("Access-Control-Allow-Credentials", true)
 
-    next()
-})
+  // Website you wish to allow to connect
 
-app.use(express.json({limit: "1mb"}))
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+
+
+  // Request methods you wish to allow
+
+  res.setHeader(
+
+    "Access-Control-Allow-Methods",
+
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+
+  );
+
+
+
+  // Request headers you wish to allow
+
+  res.setHeader(
+
+    "Access-Control-Allow-Headers",
+
+    "Content-Type, Authorization, Accept, Accept-Language, X-Authorization, X-Requested-With,content-type"
+
+  );
+
+
+
+  // Set to true if you need the website to include cookies in the requests sent
+
+  // to the API (e.g. in case you use sessions)
+
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+
+
+  // Pass to next layer of middleware
+
+  next();
+
+});
+
+app.use(express.json({ limit: "1mb" }));
 
 app.use(UserRouter)
 app.use(initRouter)
